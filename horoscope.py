0a4a69069a0a8ca6518909f162b7a60a19df0c3a
Acid Wizard <adwiz@yandex.ru>
2021-02-08 11:48:33 +0300

    horoscope added

---
+++ horoscope.py
@@ -0,0 +1,23 @@
+import random as r
+from pprint import pprint
+
+
+def generate_prophecies():
+
+    generated_prophecies = []
+    i = 0
+    while i < 5:
+        times = ["утром", "днём", "вечером", "ночью", "после обеда", "перед сном"]
+        advices = ["ожидайте", "предостерегайтесь", "будьте открыты для"]
+        promises = ["гостей из забытого прошлого", "встреч со старыми знакомыми", "неожиданного праздника",
+                    "приятных перемен"]
+        time = r.choice(times)
+        advices = r.choice(advices)
+        promises = r.choice(promises)
+        generated_prophecies.append(time.capitalize() + ' ' + advices + ' ' + promises + '.')
+        i += 1
+    return generated_prophecies
+
+
+horoscope = generate_prophecies()
+pprint(horoscope)
