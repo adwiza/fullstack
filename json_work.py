2ff72a641e4ebfed7af81f0f1198b32193f6b027
Acid Wizard <adwiz@yandex.ru>
2021-02-12 16:37:48 +0300

    module 5 completed

--- json_work.py
+++ json_work.py
@@ -1,5 +1,3 @@
-from pprint import pprint
-
 import pandas as pd
 import json
 import collections
@@ -12,9 +10,29 @@ for addr in data['ip']:
 
 total_visits = 0
 visits = count.values()
+most_common_visitor = count.most_common(1)
 for visit in visits:
     total_visits += int(visit)
 
-most_common_visitor = count.most_common(1)
-print(int(most_common_visitor[0][1]) / total_visits * 100)
+my_list = []
+for i in data.values:
+    if i[1] == most_common_visitor[0][0]:
+        my_list.append(i)
 
+last_one = my_list[-1]
+last_one_user_agent = ''.join(last_one[2:])
+time_stamp_of_last_one = ''.join(last_one[0])
+
+suspicious_agent = {
+    'ip': most_common_visitor[0][0],
+    'fraction': round(int(most_common_visitor[0][1]) * 100 / total_visits, 2),
+    'count': total_visits,
+    'last':
+        {'agent': last_one_user_agent,
+         'timestamp': time_stamp_of_last_one,
+         },
+}
+
+with open('data/suspicious_agent.json', 'w') as outfile:
+    json.dump(suspicious_agent, outfile, indent=4)
+print(json.dumps(suspicious_agent, indent=4))
