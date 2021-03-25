847c45f5fdd0708d2049b60e85e7895d6ef78e31
Acid Wizard <adwiz@yandex.ru>
2021-03-25 21:35:38 +0300

    module 10 task 3 begin

--- todoapp/tasks/urls.py
+++ todoapp/tasks/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('list/', views.tasks_list, name='list'),
+    path('create/', views.task_create, name='create'),
+    path('add-task/', views.add_task, name='api-add-task'),
+    path('complete/<int:uid>', views.complete_task, name='complete'),
+    path('delete/<int:uid>', views.delete_task, name='delete'),
 ]
--- todoapp/tasks/views.py
+++ todoapp/tasks/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from tasks.models import TodoItem
 
 # Create your views here.
@@ -7,6 +7,28 @@ from tasks.models import TodoItem
 
 def index(request):
     return HttpResponse('Примитивный ответ из приложения tasks')
+
+
+def complete_task(request, uid):
+    print(uid)
+    return HttpResponse('OK')
+
+
+def add_task(request):
+    if request.method == 'POST':
+        desc = request.POST['description']
+        t = TodoItem(description=desc)
+        t.save()
+    return redirect('/tasks/list')
+
+
+def delete_task(request, uid):
+    print(uid)
+    return redirect('/tasks/list')
+
+
+def task_create(request):
+    return render(request, 'tasks/create.html')
 
 
 def tasks_list(request):
